e757402cd14d4cad406a09799f9be856baa2dd85
JaretEduardo <kaiilawlietmx@gmail.com>
2025-06-30 21:03:31 -0600

    fix/home-component

--- TojoFront/src/app/layouts/home/home.component.ts
+++ TojoFront/src/app/layouts/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarAuthComponent } from '../../shared/navbarauth/navbarauth.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { CategoriesComponent } from '../../components/categories/categories.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarAuthComponent, HeroComponent, CategoriesComponent],
+  imports: [NavbarComponent, HeroComponent, CategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

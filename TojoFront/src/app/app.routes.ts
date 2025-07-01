863a0c4f3e8e4764b2332a878fc2d2639c363fca
JaretEduardo <kaiilawlietmx@gmail.com>
2025-06-30 19:52:36 -0600

    feat/body-navbar

--- TojoFront/src/app/app.routes.ts
+++ TojoFront/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layouts/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent,
+        children: [
+            { path: '', component: HeroComponent },
+            { path: '', component: CategoriesComponent }
+        ]
+    }
+];

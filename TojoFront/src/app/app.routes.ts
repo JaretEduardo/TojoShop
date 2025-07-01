4a396737f2766b2ca752acab0c15b6a53a7f140f
JaretEduardo <kaiilawlietmx@gmail.com>
2025-06-30 20:46:24 -0600

    fix/rename-navbar-component

--- TojoFront/src/app/app.routes.ts
+++ TojoFront/src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './layouts/home/home.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { CategoriesComponent } from './components/categories/categories.component';
+import { NavbarAuthComponent } from './shared/navbarauth/navbarauth.component';
 
 export const routes: Routes = [
     {
         path: '', component: HomeComponent,
-        /*children: [
-            { path: '', component: HeroComponent },
-            { path: '', component: CategoriesComponent }
-        ]*/
+        children: [
+            /*{ path: '', component: NavbarAuthComponent }*/
+        ]
+    },
+    {
+        path: 'home', component: HomeComponent,
+        children: [
+            /*{ path: '', component: HeroComponent }*/
+        ]
     }
 ];
--- TojoFront/src/app/layouts/home/home.component.ts
+++ TojoFront/src/app/layouts/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { NavbarAuthComponent } from '../../shared/navbarauth/navbarauth.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { CategoriesComponent } from '../../components/categories/categories.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, CategoriesComponent],
+  imports: [NavbarAuthComponent, HeroComponent, CategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

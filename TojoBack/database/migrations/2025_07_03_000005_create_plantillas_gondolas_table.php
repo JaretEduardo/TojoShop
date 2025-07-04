a3f1f1ed332a92cd245e9de9e1e9fd1942cdd8b4
Jaret Eduardo Gonzalez Carrasco <jaret.gonzalez.ct59@gmail.com>
2025-07-03 19:25:27 -0600

    fix/plantillas-gondolas

--- TojoBack/database/migrations/2025_07_03_000005_create_plantillas_gondolas_table.php
+++ TojoBack/database/migrations/2025_07_03_000005_create_plantillas_gondolas_table.php
@@ -12,10 +12,20 @@ return new class extends Migration
             $table->id('plantilla_id');
             $table->unsignedBigInteger('sucursal_id');
             $table->string('nombre_gondola', 100);
-            $table->text('descripcion')->nullable();
+            $table->string('nombre_producto', 100);
+            $table->unsignedBigInteger('producto_id');
+            $table->integer('posicion_x');
+            $table->integer('posicion_y');
             $table->timestamps();
 
-            $table->foreign('sucursal_id')->references('sucursal_id')->on('sucursales')->onDelete('cascade');
+            $table->foreign('sucursal_id')
+                ->references('sucursal_id')
+                ->on('sucursales')
+                ->onDelete('cascade');
+            $table->foreign('producto_id')
+                ->references('producto_id')
+                ->on('productos')
+                ->onDelete('cascade');
         });
     }
 

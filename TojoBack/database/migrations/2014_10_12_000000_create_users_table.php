207c2443baa21a2b5222a5b42cb340d93aea79f8
JaretEduardo <kaiilawlietmx@gmail.com>
2025-07-03 19:45:50 -0600

    feat/modify-users-migration

--- TojoBack/database/migrations/2014_10_12_000000_create_users_table.php
+++ TojoBack/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,25 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('rol', ['usuario', 'empleado', 'encargado'])->default('usuario');
+            $table->string('rfid_tag', 50)->nullable();
+            $table->unsignedBigInteger('sucursal_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('sucursal_id')->references('sucursal_id')->on('sucursales')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
